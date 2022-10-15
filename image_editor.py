868a387b13daef268ee8ebced576448f6739d997
jhash20 <114323893+jhash20@users.noreply.github.com>
2022-10-15 15:35:57 -0700

    Update image_editor.py
    Initialized variables so that the script can be more easily repurposed for future use.

--- image_editor.py
+++ image_editor.py
@@ -4,7 +4,8 @@
 import os, sys
 from PIL import Image
 
-new_path = "/opt/icons/"
+src_path = os.path.expanduser('~') + "/images/"
+dst_path = "/opt/icons/"
 
 os.chdir(directory)
 # iterate over files in that directory
@@ -12,14 +13,16 @@ for root, dirs, files in os.walk(".")
     for file in files:
         # splits the path name into a pair root and ext
         f, e = os.path.splitext(file)
+        infile = os.path.join(src_path,f)
+        outfile = os.path.join(dst_path,f)
         try:
             # open image
-            im = Image.open(file)
+            im = Image.open(infile)
             # modify image
             new_im = im.resize((128,128))
             new_im = im.rotate(-90)
             new_im = im.convert('RGB')
             # save image as JPEG in correct directory
-            new_im = im.save(new_path + str(f) + ".jpg")
+            new_im = im.save(outfile + ".jpg")
         except OSError:
             print("cannot convert", infile)
