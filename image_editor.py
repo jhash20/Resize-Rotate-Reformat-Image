a4387b09f23fe22793f05e94cd7c14f2cde32494
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-19 12:48:25 -0700

    Update image_editor.py: Minor changes
    Minor changes:
    Added script description 
    Added more comment lines to be more descriptive/helpful
    Add IOError to exceptions

--- image_editor.py
+++ image_editor.py
@@ -4,7 +4,13 @@
 import os, sys
 from PIL import Image
 
-# initializes variables
+"""
+Script to iterate over directory of images in order to modify them to specifications for upload:
+128x128 image resolution, 90 degrees clockwise, .jpeg format
+Then save them into a different directory
+"""
+
+# initializes source and destination paths
 src_path = os.path.expanduser('~') + "/images/"
 dst_path = "/opt/icons/"
 
@@ -12,18 +18,20 @@ os.chdir(src_path)
 # iterates over files in that directory
 for root, dirs, files in os.walk("."):
     for file in files:
-        # splits the path name into a pair root and ext
+        # splits the path name into a pair file name and ext
         f, e = os.path.splitext(file)
+        # initializes source and destination file paths using path and file name
         infile = os.path.join(src_path,f)
         outfile = os.path.join(dst_path,f)
         try:
             # opens image
             with Image.open(infile) as im:
-                # modifies image
+                # modifies image to specified image resolution, orientation, and format
                 new_im = im.resize((128,128))
                 new_im = new_im.rotate(-90)
                 new_im = new_im.convert('RGB')
                 # saves image as JPEG in correct directory
-                new_im = new_im.save(outfile + ".jpg")
-        except OSError:
-            print("cannot convert", infile)
+                new_im = new_im.save(outfile + ".jpeg")
+        # raises and prints an exception if error occurs
+        except (IOError, OSError):
+            print("Cannot convert", infile)
