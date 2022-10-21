aa493ac861047f900e738e24fd214fdee1a9c843
Justin Hashimoto <114323893+jhash20@users.noreply.github.com>
2022-10-20 19:09:16 -0700

    Formatting per PEP
    DocString before imports
    Removed unnecessary comments
    Made comments more useful
    Standardized usage of quotation marks

--- image_editor.py
+++ image_editor.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
 
-# imports required modules
+"""Script that processes directory of images and modifies to specifications for upload:
+128x128 image resolution, 90 degrees clockwise, .jpeg format
+Then saves them into a different directory
+"""
+
 import os, sys
 from PIL import Image
 
-"""
-Script to iterate over directory of images in order to modify them to specifications for upload:
-128x128 image resolution, 90 degrees clockwise, .jpeg format
-Then save them into a different directory
-"""
 
-# initializes source and destination paths
 src_path = os.path.expanduser('~') + "/images/"
 dst_path = "/opt/icons/"
-
 os.chdir(src_path)
-# iterates over files in that directory
+
+# iterates over files in directory
 for root, dirs, files in os.walk("."):
     for file in files:
-        # splits the path name into a pair file name and ext
         f, e = os.path.splitext(file)
-        # initializes source and destination file paths using path and file name
         infile = os.path.join(src_path,f)
         outfile = os.path.join(dst_path,f)
         try:
-            # opens image
+            # opens/modifies image to specifications
             with Image.open(infile) as im:
-                # modifies image to specified image resolution, orientation, and format
                 new_im = im.resize((128,128))
                 new_im = new_im.rotate(-90)
-                new_im = new_im.convert('RGB')
+                new_im = new_im.convert("RGB")
                 # saves image as JPEG in correct directory
                 new_im = new_im.save(outfile + ".jpeg")
-        # raises and prints an exception if error occurs
         except (IOError, OSError):
             print("Cannot convert", infile)
